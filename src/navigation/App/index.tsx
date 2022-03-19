ef89ad21ca3ea6fadaeee86d6ce6cf4e858b41b9
Anton Marchenko <anton.mrch1998@mail.ru>
2022-03-19 15:25:46 +0300

    first commit

--- src/navigation/App/index.tsx
+++ src/navigation/App/index.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: '#e91e63',
+        tabBarActiveTintColor: '#e91e43',
         headerShown: false,
       }}
     >
